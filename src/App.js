69ee7e76d96e632b45da388ae683f3fe39e0054f
MisterHuh <jaehuh86@yahoo.com>
2020-07-06 20:40:54 -0700

    material ui practice

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import 'fontsource-roboto';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    // height: '100vh',
+    width: 200,
+    border: '1px solid black'
+  }
+}));
+
 function App() {
+  const classes = useStyles;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <form className={classes.root}>
+
+      <TextField id="" margin="normal" variant="outlined" label="first name" />
+      <TextField id="" margin="normal" variant="outlined" label="last name" />
+      <TextField id="" margin="normal" fullWidth variant="outlined" label="email" />
+      <TextField id="" margin="normal" fullWidth variant="outlined" label="message" />
+      <div>
+        <Button type="submit" variant="contained" color="primary">Submit</Button>
+      </div>
+
+    </form>
+
   );
 }
 
